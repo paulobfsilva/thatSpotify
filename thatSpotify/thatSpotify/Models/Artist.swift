669e13fec056e8009e1a037c7b8c787596f6a264
Paulo Silva <paulobfsilva@gmail.com>
2021-10-16 21:44:16 +0100

    Initial model stubs

---
+++ thatSpotify/thatSpotify/Models/Artist.swift
@@ -0,0 +1,8 @@
+//
+//  Artist.swift
+//  thatSpotify
+//
+//  Created by Paulo Silva on 16/10/2021.
+//
+
+import Foundation
---
+++ thatSpotify/thatSpotify/Models/Playlist.swift
@@ -0,0 +1,8 @@
+//
+//  Playlist.swift
+//  thatSpotify
+//
+//  Created by Paulo Silva on 16/10/2021.
+//
+
+import Foundation
