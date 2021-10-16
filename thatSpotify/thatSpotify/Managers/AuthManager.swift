8d97d320db855c0f9d361d81a93ef776532cacb1
Paulo Silva <paulobfsilva@gmail.com>
2021-10-16 21:43:13 +0100

    Initial Manager files stubs

---
+++ thatSpotify/thatSpotify/Managers/AuthManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthManager.swift
+//  thatSpotify
+//
+//  Created by Paulo Silva on 16/10/2021.
+//
+
+import Foundation
---
+++ thatSpotify/thatSpotify/Managers/HapticsManager.swift
@@ -0,0 +1,8 @@
+//
+//  HapticsManager.swift
+//  thatSpotify
+//
+//  Created by Paulo Silva on 16/10/2021.
+//
+
+import Foundation
