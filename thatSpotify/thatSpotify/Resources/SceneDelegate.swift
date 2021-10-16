46e1a086d58cedc6b2fc09511d3e4e77db059662
Paulo Silva <paulobfsilva@gmail.com>
2021-10-16 22:00:22 +0100

    setup welcomeVC

--- thatSpotify/thatSpotify/Resources/SceneDelegate.swift
+++ thatSpotify/thatSpotify/Resources/SceneDelegate.swift
@@ -15,8 +15,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
+        if AuthManager.shared.isSignedIn {
+            window.rootViewController = TabBarViewController()
+        } else {
+            let navVC = UINavigationController(rootViewController: WelcomeViewController())
+            navVC.navigationBar.prefersLargeTitles = true
+            navVC.viewControllers.first?.navigationItem.largeTitleDisplayMode = .always
+            window.rootViewController = navVC
+        }
         window.makeKeyAndVisible()
-        window.rootViewController = TabBarViewController()
         self.window = window
         
     }
