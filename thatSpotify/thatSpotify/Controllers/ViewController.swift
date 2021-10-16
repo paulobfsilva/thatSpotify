3c534e634006aa9b829c75315d0e65573031d10e
Paulo Silva <paulobfsilva@gmail.com>
2021-10-16 21:37:40 +0100

    Initial project setup, including removing Storyboard and setup app and scene Delegates

---
+++ thatSpotify/thatSpotify/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  thatSpotify
+//
+//  Created by Paulo Silva on 16/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        view.backgroundColor = .red
+    }
+
+}
+
