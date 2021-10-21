d49edc8efa74c478fc4a164a34b9f893af4f8917
Paulo Silva <paulobfsilva@gmail.com>
2021-10-21 22:12:29 +0100

    initial profile implementation

--- thatSpotify/thatSpotify/Controllers/Other/ProfileViewController.swift
+++ thatSpotify/thatSpotify/Controllers/Other/ProfileViewController.swift
@@ -7,14 +7,30 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.isHidden = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    private var models = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Profile"
+        view.addSubview(tableView)
         fetchProfile()
         view.backgroundColor = .systemBackground
-        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
     
     private func fetchProfile() {
@@ -32,7 +48,14 @@ class ProfileViewController: UIViewController {
     }
     
     private func updateUI(with model: UserProfile) {
+        tableView.isHidden = false
+        // configure table models
+        models.append("Full Name: \(model.display_name)")
+        models.append("Email Address: \(model.email)")
+        models.append("User ID: \(model.id)")
+        models.append("Plan: \(model.product)")
         
+        tableView.reloadData()
     }
     
     private func failedToGetProfile() {
@@ -42,6 +65,18 @@ class ProfileViewController: UIViewController {
         label.textColor = .secondaryLabel
         view.addSubview(label)
         label.center = view.center
+    }
+    
+    // MARK: - TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row]
+        cell.selectionStyle = .none
+        return cell
     }
 
 }
