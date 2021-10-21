13609d18be3520ba3e570d547b035de9ca4bbad5
Paulo Silva <paulobfsilva@gmail.com>
2021-10-21 21:57:08 +0100

    Show error in case profile fails to load

--- thatSpotify/thatSpotify/Controllers/Other/ProfileViewController.swift
+++ thatSpotify/thatSpotify/Controllers/Other/ProfileViewController.swift
@@ -11,16 +11,37 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         title = "Profile"
-        APICaller.shared.getCurrentUserProfile { result in
-            switch result {
-            case .success(let model):
-                break
-            case .failure(let error):
-                print(error.localizedDescription)
+        fetchProfile()
+        view.backgroundColor = .systemBackground
+        
+    }
+    
+    private func fetchProfile() {
+        APICaller.shared.getCurrentUserProfile { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let model):
+                    self?.updateUI(with: model)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    self?.failedToGetProfile()
+                }
             }
         }
+    }
+    
+    private func updateUI(with model: UserProfile) {
+        
+    }
+    
+    private func failedToGetProfile() {
+        let label = UILabel(frame: .zero)
+        label.text = "Failed to load profile"
+        label.sizeToFit()
+        label.textColor = .secondaryLabel
+        view.addSubview(label)
+        label.center = view.center
     }
 
 }
