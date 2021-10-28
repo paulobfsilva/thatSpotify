b63ffa33e897da52abf54ca15048d5398353c31e
Paulo Silva <paulobfsilva@gmail.com>
2021-10-28 22:24:38 +0100

    initial UI for Home VC with UICollectionViewLayout

--- thatSpotify/thatSpotify/Controllers/Core/HomeViewController.swift
+++ thatSpotify/thatSpotify/Controllers/Core/HomeViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    private var collectionView: UICollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewCompositionalLayout { sectionIndex, _ -> NSCollectionLayoutSection? in
+            return HomeViewController.createSectionLayout(section: sectionIndex)
+        }
+    )
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.tintColor = .label
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Browse"
@@ -19,8 +33,45 @@ class HomeViewController: UIViewController {
             target: self,
             action: #selector(didTapSettings)
         )
-        
+        configureCollectionView()
+        view.addSubview(spinner)
         fetchData()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
+    
+    private func configureCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = .systemBackground
+    }
+    
+    private static func createSectionLayout(section: Int) -> NSCollectionLayoutSection {
+        // Item
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(1.0)
+            )
+        )
+        item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+        // Group
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(120)
+            ),
+            subitem: item,
+            count: 1
+        )
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+        return section
     }
     
     @objc func didTapSettings() {
@@ -31,6 +82,9 @@ class HomeViewController: UIViewController {
     }
     
     private func fetchData() {
+        // New Releases
+        // Featured Playlists,
+        // Recommended Tracks,
         APICaller.shared.getRecommendedGenres{ result in
             switch result {
             case .success(let model):
@@ -50,5 +104,19 @@ class HomeViewController: UIViewController {
         }
     }
 
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemGreen
+        return cell
+    }
+    
+    
 }
 
