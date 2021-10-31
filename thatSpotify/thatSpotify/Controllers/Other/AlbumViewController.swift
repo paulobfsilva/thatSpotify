4b249685ebc39cce0f833e01173952cf3a4e8c96
Paulo Silva <paulobfsilva@gmail.com>
2021-10-31 21:41:26 +0000

    Network requests for Album and Playlist details

--- thatSpotify/thatSpotify/Controllers/Other/AlbumViewController.swift
+++ thatSpotify/thatSpotify/Controllers/Other/AlbumViewController.swift
@@ -24,6 +24,16 @@ class AlbumViewController: UIViewController {
         super.viewDidLoad()
         title = album.name
         view.backgroundColor = .systemBackground
+        APICaller.shared.getAlbumDetails(for: album) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let model):
+                    break
+                case .failure(let error):
+                    break
+                }
+            }
+        }
     }
    
 
---
+++ thatSpotify/thatSpotify/Models/PlaylistDetailsResponse.swift
@@ -0,0 +1,25 @@
+//
+//  PlaylistDetailsResponse.swift
+//  thatSpotify
+//
+//  Created by Paulo Silva on 31/10/2021.
+//
+
+import Foundation
+
+struct PlaylistDetailsResponse: Codable {
+    let description: String
+    let external_urls: [String: String]
+    let id: String
+    let images: [APIImage]
+    let name: String
+    let tracks: PlaylistTracksResponse
+}
+
+struct PlaylistTracksResponse: Codable {
+    let items: [PlaylistItem]
+}
+
+struct PlaylistItem: Codable {
+    let track: AudioTrack
+}
